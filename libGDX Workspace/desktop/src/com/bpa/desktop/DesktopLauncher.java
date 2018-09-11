ec7856fc067c6e71ca333e81df9ffae6f10c0736
MisterGoatt <richardhutch80@hotmail.com>
2018-09-12 07:46:16 +1100

    Added workplace files

---
+++ libGDX Workspace/desktop/src/com/bpa/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bpa.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bpa.Start;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Start(), config);
+	}
+}
