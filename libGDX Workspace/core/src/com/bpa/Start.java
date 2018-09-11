de4f3f66611826ba28b1231f411bf61f6dcdab51
MisterGoatt <richardhutch80@hotmail.com>
2018-09-11 16:00:49 -0500

    Added SillyBoi.jpg

--- libGDX Workspace/core/src/com/bpa/Start.java
+++ libGDX Workspace/core/src/com/bpa/Start.java
@@ -13,7 +13,7 @@ public class Start extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("SillyBoi.jpg");
 	}
 
 	@Override
