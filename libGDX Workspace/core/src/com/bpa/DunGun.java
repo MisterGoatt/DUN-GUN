ad091c452712e0c162d29ad561c09d92e58c60c4
Richard Hutcheson <richardhutch80@gmail.com>
2018-09-18 15:40:41 -0500

    Work on moving code around to MainMenu

--- libGDX Workspace/core/src/com/bpa/DunGun.java
+++ libGDX Workspace/core/src/com/bpa/DunGun.java
@@ -13,58 +13,39 @@ import com.badlogic.gdx.Game;
 
 public class DunGun extends Game{
 	public SpriteBatch batch;
-	Texture publisherScreen;
+	/*Texture publisherScreen;
 	Texture creditScreen;
 	Texture titleScreen;
 	private long startTime = System.currentTimeMillis();
 	private long counter;
-	BitmapFont framerate; //font for frame rate display
+	BitmapFont framerate; //font for frame rate display*/
 	private OrthographicCamera camera;
 
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		publisherScreen = new Texture("screens/ctm_placeholder.jpg");
+		/*publisherScreen = new Texture("screens/ctm_placeholder.jpg");
 		creditScreen = new Texture("screens/credits_placeholder.jpg");
 		titleScreen = new Texture("screens/titlescreen_placeholder.jpg");
-		framerate = new BitmapFont(Gdx.files.internal("fonts/CourierNew32.fnt"));
+		framerate = new BitmapFont(Gdx.files.internal("fonts/CourierNew32.fnt"));*/
 		this.setScreen(new MainMenu(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1); //background in RGBA
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); //clears the screen
-		
-		//System.out.println((System.currentTimeMillis() - startTime) / 1000);
-		counter = (System.currentTimeMillis() - startTime) / 1000;
-		batch.begin(); //begins sprite batch
 
-		//draws publisher screen, credit screen, and then title screen
-		if (counter <= 2) {
-			batch.draw(publisherScreen, 0, 0);
-		}else if (counter <=  5 && counter > 2) {
-			batch.draw(creditScreen, 0, 0);
-		}else if (counter >= 5.1 && counter <= 7.1){
-			batch.draw(titleScreen, 0, 0);
-		}else {
-			super.render(); //REMEMBER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		}
-		int f = Gdx.graphics.getFramesPerSecond(); // grabs frames per second
-		String frames = Integer.toString(f); //converts frames per second to a string
-		framerate.draw(batch, frames, 5, 785); //displays frames per second as text in top left
-		batch.end(); //what actually displays everything to the screen
+		super.render(); //REMEMBER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		publisherScreen.dispose();
+		/*publisherScreen.dispose();
 		creditScreen.dispose();
 		titleScreen.dispose();
-		framerate.dispose();
+		framerate.dispose();*/
 		
 	}
 }
--- libGDX Workspace/core/src/com/bpa/MainMenu.java
+++ libGDX Workspace/core/src/com/bpa/MainMenu.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 
 public class MainMenu implements Screen{
 	
@@ -11,13 +13,22 @@ public class MainMenu implements Screen{
 	
 	OrthographicCamera camera;
 
+	private long startTime = System.currentTimeMillis();
+	private long counter;
+	BitmapFont framerate; //font for frame rate display
+	Texture publisherScreen;
+	Texture creditScreen;
+	Texture titleScreen;
 	
 	public MainMenu(final DunGun game) {
 		this.game = game;
 
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 480);
-
+		camera.setToOrtho(false, 1000, 800);
+		publisherScreen = new Texture("screens/ctm_placeholder.jpg");
+		creditScreen = new Texture("screens/credits_placeholder.jpg");
+		titleScreen = new Texture("screens/titlescreen_placeholder.jpg");
+		framerate = new BitmapFont(Gdx.files.internal("fonts/CourierNew32.fnt"));
 	}
 	
 	@Override
@@ -26,10 +37,30 @@ public class MainMenu implements Screen{
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		camera.update();
+
 		game.batch.setProjectionMatrix(camera.combined);
 
-		//game.batch.begin();
-		//game.batch.end();
+		game.batch.begin();
+		Gdx.gl.glClearColor(0, 0, 0, 1); //background in RGBA
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); //clears the screen
+		
+		//System.out.println((System.currentTimeMillis() - startTime) / 1000);
+		counter = (System.currentTimeMillis() - startTime) / 1000;
+		game.batch.begin(); //begins sprite batch
+
+		//draws publisher screen, credit screen, and then title screen
+		if (counter <= 2) {
+			game.batch.draw(publisherScreen, 0, 0);
+		}else if (counter <=  5 && counter > 2) {
+			game.batch.draw(creditScreen, 0, 0);
+		}else if (counter >= 5.1 && counter <= 7.1){
+			game.batch.draw(titleScreen, 0, 0);
+		}
+		int f = Gdx.graphics.getFramesPerSecond(); // grabs frames per second
+		String frames = Integer.toString(f); //converts frames per second to a string
+		framerate.draw(game.batch, frames, 5, 785); //displays frames per second as text in top left
+		game.batch.end(); //what actually displays everything to the screen
+		game.batch.end();
 
 	}
 
@@ -66,7 +97,11 @@ public class MainMenu implements Screen{
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
-		
+		game.batch.dispose();
+		publisherScreen.dispose();
+		creditScreen.dispose();
+		titleScreen.dispose();
+		framerate.dispose();
 	}
 
 }
