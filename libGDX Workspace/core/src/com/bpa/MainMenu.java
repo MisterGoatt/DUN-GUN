df873f230328d003e4844be8b0bfd505521ad717
MisterGoatt <richardhutch80@hotmail.com>
2018-09-20 17:19:14 -0500

    Added skip intro screens and improved the fun joke

--- libGDX Workspace/core/src/com/bpa/MainMenu.java
+++ libGDX Workspace/core/src/com/bpa/MainMenu.java
@@ -18,13 +18,14 @@ public class MainMenu implements Screen{
 	private long startTime = System.currentTimeMillis();
 	private long counter;
 	BitmapFont framerate; //font for frame rate display
+	BitmapFont dvf;
 	Texture publisherScreen;
 	Texture creditScreen;
 	Texture titleScreen;
 	Texture dv;
+	boolean unlimiter = false;
 	boolean isPressed = false; //for mouse input spam
-	BitmapFont dvf;
-
+	boolean skip1 = false;
 
 
 	
@@ -44,47 +45,70 @@ public class MainMenu implements Screen{
 	@Override
 	public void render(float delta) {
 
+		game.batch.begin(); //**************************
+
 		int mX = Gdx.input.getX();
 		int mY = Gdx.input.getY();
 		
 		camera.update();
-
 		game.batch.setProjectionMatrix(camera.combined);
-
-		game.batch.begin();
 		Gdx.gl.glClearColor(0, 0, 0, 1); //background in RGBA
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); //clears the screen
 		
 		//System.out.println((System.currentTimeMillis() - startTime) / 1000);
 		counter = (System.currentTimeMillis() - startTime) / 1000;
+		
 		//Mouse Input
-		if (Gdx.input.isButtonPressed(Input.Buttons.LEFT) && (isPressed == false)) {
+		if (Gdx.input.isButtonPressed(Input.Buttons.LEFT) && (isPressed == false) && unlimiter == false){
 			isPressed = true;
 			if (mX >= 700 && mX <= 800 && mY >= 300 &&  mY <= 400) {
-				System.out.println("DEUS VULT");
 				game.batch.draw(dv, mX, mY);
-				dvf.draw(game.batch, "DEUS VULT", mX, mY - 100);
+				dvf.draw(game.batch, "DEUS VULT", mX, mY - 50);
+			}
+		}else if (Gdx.input.isButtonPressed(Input.Buttons.LEFT) && unlimiter == true)  {
+			if (mX >= 700 && mX <= 800 && mY >= 300 &&  mY <= 400) {
+				game.batch.draw(dv, mX, mY);
+				dvf.draw(game.batch, "DEUS VULT", mX, mY - 50);
 			}
 		}
-		//keeps from holding down spam
-		if (!(Gdx.input.isButtonPressed(Input.Buttons.LEFT))) {
-			
-			isPressed = false;
+		//turns off mouse input limiter
+		if (Gdx.input.isKeyJustPressed(Input.Keys.P)) {
+
+			if (unlimiter == false){
+				unlimiter = true;
+				
+			}else if (unlimiter == true) {
+				unlimiter = false;
+			}
+
 		}
+		
+		//mouse input limiter
+		if (unlimiter == false){
+			if (!(Gdx.input.isButtonPressed(Input.Buttons.LEFT))) {
+				isPressed = false;
+			}			
+		}
+
 		//draws publisher screen, credit screen, and then title screen
-		if (counter <= 2) {
-			game.batch.draw(publisherScreen, 0, 0);
-		}else if (counter <=  5 && counter > 2) {
-			game.batch.draw(creditScreen, 0, 0);
-		}else if (counter >= 5.1 && counter <= 7.1){
-			game.batch.draw(titleScreen, 0, 0);
+		if (counter <= 7.1 && Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
+			skip1 = true;
 		}
+		if (skip1 == false) {
+			if (counter <= 2) {
+					game.batch.draw(publisherScreen, 0, 0);
+				}else if (counter <=  5 && counter > 2) {
+					game.batch.draw(creditScreen, 0, 0);
+				}else if (counter >= 5.1 && counter <= 7.1){
+					game.batch.draw(titleScreen, 0, 0);
+				}
+	}
 		int f = Gdx.graphics.getFramesPerSecond(); // grabs frames per second
 		String frames = Integer.toString(f); //converts frames per second to a string
 		framerate.draw(game.batch, frames, 5, 785); //displays frames per second as text in top left
-		game.batch.end(); //what actually displays everything to the screen
 		System.out.println(mX + " " + mY);
 
+		game.batch.end(); //*************************
 		
 	}
 
