11de848c44ca57a7ab59a04ff9992b34552c39a1
Kyle <Kyle@192.168.0.131>
2018-09-27 19:42:29 -0500

    Implemented Tiled MaP

--- libGDX Workspace/core/src/com/bpa/DunGun.java
+++ libGDX Workspace/core/src/com/bpa/DunGun.java
@@ -1,22 +1,32 @@
 package com.bpa;
 
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.bpa.world.GameMap;
+import com.bpa.world.TiledGameMap;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 
 
 public class DunGun extends Game{
 	public SpriteBatch batch;
-
+	OrthographicCamera cam;
+	GameMap gameMap;
+	Texture img;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		
+		
 
 		this.setScreen(new MainMenu(this));
 	}
 
 	@Override
 	public void render () {
-
+		
 		super.render(); //REMEMBER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 	}
@@ -24,6 +34,7 @@ public class DunGun extends Game{
 	@Override
 	public void dispose () {
 		batch.dispose();
+		
 
 	}
 }
--- libGDX Workspace/core/src/com/bpa/MainMenu.java
+++ libGDX Workspace/core/src/com/bpa/MainMenu.java
@@ -6,6 +6,8 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.bpa.world.GameMap;
+import com.bpa.world.TiledGameMap;
 import com.badlogic.gdx.Input;
 
 
@@ -13,7 +15,7 @@ public class MainMenu implements Screen{
 	
 	final DunGun game;
 	
-	OrthographicCamera camera;
+//	OrthographicCamera camera;
 
 	private long startTime = System.currentTimeMillis();
 	private long counter;
@@ -26,15 +28,23 @@ public class MainMenu implements Screen{
 	Texture dv;
 	boolean unlimiter = false;
 	boolean isPressed = false; //for mouse input spam
+	boolean a = false;
 	boolean skip1 = false;
-
-
+	Texture img;
+	OrthographicCamera cam;
+	GameMap gameMap;
+	
 	
 	public MainMenu(final DunGun game) {
 		this.game = game;
-
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 1500, 800);
+		
+		cam = new OrthographicCamera();
+		cam.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		
+		gameMap = new TiledGameMap();
+		
+		
+		
 		publisherScreen = new Texture("screens/ctm_placeholder.jpg");
 		creditScreen = new Texture("screens/credits_placeholder.jpg");
 		titleScreen = new Texture("screens/titlescreen_placeholder.jpg");
@@ -42,63 +52,43 @@ public class MainMenu implements Screen{
 		framerate = new BitmapFont(Gdx.files.internal("fonts/CourierNew32.fnt"));
 		dv = new Texture("hqdefault.jpg");
 		dvf = new BitmapFont(Gdx.files.internal("fonts/CourierNew32.fnt"));
+		
 	}
 	
 	@Override
 	public void render(float delta) {
 
 		game.batch.begin(); 
-
+		
+		
+		
+		
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		
 		//mouse x and y
 		int mX = Gdx.input.getX();
 		int mY = Gdx.input.getY();
 		
-		camera.update();
-		game.batch.setProjectionMatrix(camera.combined);
-		Gdx.gl.glClearColor(0, 0, 0, 1); //background in RGBA
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); //clears the screen	
 		
-		//MOUSE INPUT
-		//**********************************
-		if (Gdx.input.isButtonPressed(Input.Buttons.LEFT) && (isPressed == false) && unlimiter == false){
-			isPressed = true;
-			if (mX >= 700 && mX <= 800 && mY >= 300 &&  mY <= 400) {
-				game.batch.draw(dv, mX, mY);
-				dvf.draw(game.batch, "DEUS VULT", mX, mY - 50);
-			}
-		}else if (Gdx.input.isButtonPressed(Input.Buttons.LEFT) && unlimiter == true)  {
-			if (mX >= 700 && mX <= 800 && mY >= 300 &&  mY <= 400) {
-				game.batch.draw(dv, mX, mY);
-				dvf.draw(game.batch, "DEUS VULT", mX, mY - 50);
-			}
-		}
-		//turns off mouse input limiter
-		if (Gdx.input.isKeyJustPressed(Input.Keys.P)) {
-
-			if (unlimiter == false){
-				unlimiter = true;
-				
-			}else if (unlimiter == true) {
-				unlimiter = false;
-			}
-
-		}
 		
-		//mouse input limiter
-		if (unlimiter == false){
-			if (!(Gdx.input.isButtonPressed(Input.Buttons.LEFT))) {
-				isPressed = false;
-			}			
-		}
-		//**********************************
+		
+		
+		
+		cam.update();
+		
 
 		
 
 		//DRAWS INTRO SCREENS
 		//**********************************
-		if (counter <= 7.1 && Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
+		if (counter <= 10.2 && Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
 			skip1 = true;
+			a = true;
+			
 		}
+		
 		if (skip1 == false) {
 			if (counter <= 2) {
 					game.batch.draw(publisherScreen, 0, 0);
@@ -110,15 +100,21 @@ public class MainMenu implements Screen{
 				else if (counter >= 7.2 && counter <= 10.2){
 					game.batch.draw(titleScreen, 0, 0);
 				}
+				else if (counter >= 10.21) {
+					a = true;
+				}
+				
 	}
 		//**********************************
-
-		//System.out.println(mX + " " + mY);
+		if (a == true) {
+			gameMap.render(cam);
+		}
+		System.out.println(mX + " " + mY);
 
 		
 		//FRAMES PER SECOND
 		//**********************************
-		//System.out.println((System.currentTimeMillis() - startTime) / 1000);
+		System.out.println((System.currentTimeMillis() - startTime) / 1000);
 		counter = (System.currentTimeMillis() - startTime) / 1000;
 		int f = Gdx.graphics.getFramesPerSecond(); // grabs frames per second
 		String frames = Integer.toString(f); //converts frames per second to a string
@@ -167,6 +163,7 @@ public class MainMenu implements Screen{
 		titleScreen.dispose();
 		framerate.dispose();
 		dv.dispose();
+		img.dispose();
 	}
 
 }
