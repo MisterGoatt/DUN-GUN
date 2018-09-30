c7124b0351141d06ad3fa73650394a9027ce9dd0
MisterGoatt <richardhutch80@hotmail.com>
2018-09-30 14:50:14 -0500

    Removed L1TMLoad, TileType, & TMapLocations file & added new lvl1 map

--- libGDX Workspace/core/src/com/bpa/DunGun.java
+++ libGDX Workspace/core/src/com/bpa/DunGun.java
@@ -10,7 +10,7 @@ public class DunGun extends Game{
 	//Virtual Screen size and Box2D Scale(Pixels Per Meter)
 	public static final int V_WIDTH = 320;
 	public static final int V_HEIGHT = 320;
-	public static final float PPM = 2;
+	public static final float PPM = 100;
 	
 	
 	@Override
--- libGDX Workspace/core/src/com/bpa/Level1.java
+++ libGDX Workspace/core/src/com/bpa/Level1.java
@@ -4,11 +4,11 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.ScalingViewport;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
+
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 
@@ -16,29 +16,36 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 public class Level1 implements Screen{
 	private OrthographicCamera cam;
 	final DunGun game;
-	TMapLocations level1Map;
+	//TMapLocations level1Map;
 	private Viewport gamePort;
-
+	private TmxMapLoader maploader; //what loads map into game
+	private TiledMap map; //references map itself
+	private OrthogonalTiledMapRenderer renderer; //renders map to the screen
 	
 	public Level1(final DunGun game) {
 		this.game = game;
-		level1Map = new L1TMLoad();
+		//level1Map = new L1TMLoad();
 		cam = new OrthographicCamera();
-		//cam.setToOrtho(false, 320, 320); // centers camera on the 20x20 tile map(which equals 320x320)
+		cam.setToOrtho(false, 320, 320); // centers camera on the 20x20 tile map(which equals 320x320)
 		gamePort = new FitViewport(DunGun.V_WIDTH, DunGun.V_HEIGHT, cam); //fits view port to match map's dimensions (in this case 320x320) and scales. Adds black bars to adjust
-		cam.position.set(gamePort.getWorldWidth() /2 , gamePort.getWorldHeight() / 2, 0);
+
 		
+		maploader = new TmxMapLoader();
+		map = maploader.load("tileMaps/Tutorial/TileMap1.tmx");
+		renderer = new OrthogonalTiledMapRenderer(map);
+		
+		cam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
 	}
 
 	@Override
 	public void render(float delta) {
+		//clears screen
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		game.batch.setProjectionMatrix(cam.combined);
-		game.batch.begin(); 
+
 		//int mX = Gdx.input.getX();
 		//int mY = Gdx.graphics.getHeight() - Gdx.input.getY();
-		
 		
 		
 		if (Gdx.input.isTouched()) {
@@ -46,17 +53,18 @@ public class Level1 implements Screen{
 		}
 		
 		
-		if (Gdx.input.justTouched()) {
+		/*if (Gdx.input.justTouched()) {
 			Vector3 pos = cam.unproject(new Vector3 (Gdx.input.getX(), Gdx.input.getY(), 0)); //getting location of the screen and converts to game world coordinates
 			TileType type = level1Map.getTileTypeByLocation(0, pos.x, pos.y);
 			if (type != null) {
 				System.out.println("id: " + type.getId() + " " + type.getName() + " " + type.isCollidable() + " " + type.getDamage());
 			}
-		}
-		cam.update();
+		} */
+		//cam.update();
+		renderer.setView(cam);
+		renderer.render();
 
-		level1Map.render(cam);
-		game.batch.end();
+		//level1Map.render(cam);
 	}
 
 	@Override
@@ -85,6 +93,7 @@ public class Level1 implements Screen{
 	}
 	@Override
 	public void dispose() {
-		game.batch.dispose();
+		map.dispose();
+		renderer.dispose();
 	}
 }
