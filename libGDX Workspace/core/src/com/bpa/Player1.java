02db1f0a41713457e948fe470c947b00c1af9d0b
MisterGoatt <Richa@192.168.0.238>
2018-10-05 23:13:58 -0500

    changing author name

--- libGDX Workspace/core/src/com/bpa/Player1.java
+++ libGDX Workspace/core/src/com/bpa/Player1.java
@@ -28,9 +28,9 @@ public class Player1 extends Sprite{
 	
 	public void update(float dt) {
 		setPosition(b2body.getPosition().x - getWidth() / 2, b2body.getPosition().y - getHeight() / 2);
-	
+		System.out.println("anytbing to make commit");
 	}
-	
+	    
 	
 	public void definePlayer1() {
 		BodyDef bdef = new BodyDef();
