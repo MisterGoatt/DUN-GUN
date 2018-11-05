3201f50072c577c8a46f891ed9ff6bbd5c2945e0
Richa <Richa@192.168.0.238>
2018-11-05 17:03:51 -0600

    Implemented player rotation to follow mouse

--- libGDX Workspace/core/src/com/bpa/Level1.java
+++ libGDX Workspace/core/src/com/bpa/Level1.java
@@ -23,6 +23,8 @@ import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -30,8 +32,8 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 public class Level1 implements Screen{
 	final DunGun game;
 	//TMapLocations level1Map;
-	private OrthographicCamera cam;
-	private Viewport viewport;
+	public OrthographicCamera cam;
+	public Viewport viewport;
 	private TmxMapLoader maploader; //what loads map into game
 	private TiledMap map; 
 	private OrthogonalTiledMapRenderer mapRenderer; //renders map to the screen
@@ -40,11 +42,11 @@ public class Level1 implements Screen{
 	TextureRegion textureRegion;
 	MapLayer objectLayer;
 	private ShapeRenderer shapeRenderer;
-	//private TiledMapTileLayer terrainLayer;
-	//private int[] decorationLayersIndices;
-	private int[] layerBackround = {0, 1, 2, 3};
-	private int[] layerAfterBackground = {4};
-	private int pSpeed = 3;
+	public static Vector3 mouse_position = new Vector3(0, 0, 0);
+
+
+	//private int[] layerBackround = {0, 1, 2, 3};
+	//private int[] layerAfterBackground = {4};
 	
 	public Level1(final DunGun game) {
 		
@@ -57,23 +59,19 @@ public class Level1 implements Screen{
 		
 		cam = new OrthographicCamera();		
 		viewport = new FitViewport(DunGun.V_WIDTH, DunGun.V_HEIGHT, cam); //fits view port to match map's dimensions (in this case 320x320) and scales. Adds black bars to adjust
-		//viewport.apply();
-		       //cam.position.set((viewport.getWorldWidth() / DunGun.PPM) / 2, viewport.getWorldHeight() / DunGun.PPM, 0);
 
-		//viewport.apply();
-		//spriteBatch = new SpriteBatch();
-		textureAtlas = new TextureAtlas(Gdx.files.internal("sprites/soldier1.atlas"));
-		textureRegion = textureAtlas.findRegion("soldier1");
+		textureAtlas = new TextureAtlas(Gdx.files.internal("sprites/TDPlayer.atlas"));
+		textureRegion = textureAtlas.findRegion("TDPlayer");
 		p1 = new Player(new Sprite(textureRegion));
 		p1.setPosition((viewport.getWorldWidth() / 2), //places the p1 at the center of the camera
 				(viewport.getWorldHeight() / 2)) ;
-		//cam.position.x = (viewport.getWorldWidth() / 2) * DunGun.PPM;
-		//cam.position.y = (viewport.getWorldHeight() / 2) * DunGun.PPM;
+		
+
 		shapeRenderer = new ShapeRenderer();
         
 		
 		Gdx.input.setInputProcessor((InputProcessor) p1);
-		//cam.zoom -= .45;
+		cam.zoom -= .45;
 	}
 	
 	@Override
@@ -82,12 +80,9 @@ public class Level1 implements Screen{
 		//clears screen
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-		
 	 	if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
 			cam.zoom -= .01;
 
-			//cam.translate(Gdx.input.getDeltaX() * (-1), Gdx.input.getDeltaY());
 		}
 	 	if (Gdx.input.isButtonPressed(Input.Buttons.RIGHT)) {
 			cam.zoom += .01;
@@ -96,40 +91,33 @@ public class Level1 implements Screen{
 		
         
         mapRenderer.render();
-
+       
         shapeRenderer.setProjectionMatrix(cam.combined); //keeps circle from doing weird out of sync movement
         shapeRenderer.setColor(100, 100, 100, 0);
-        shapeRenderer.begin(ShapeType.Filled);
-        shapeRenderer.circle(p1.getX(), p1.getY(), 10);
-        shapeRenderer.end();;
+        shapeRenderer.begin(ShapeType.Line);
+        shapeRenderer.circle(p1.getX() + 16, p1.getY() + 10, 10);
+        shapeRenderer.end();
         
         cam.position.set(p1.getX() + p1.getWidth() / 2, p1.getY() + p1.getHeight()/ 2, 0);
         cam.update(); //updates orthographic camera
-
         mapRenderer.setView(cam);
         
         //render our game map
         //mapRenderer.render(); // renders map
 		//mapRenderer.render(layerBackround); //renders layer in Tiled that p1 covers
         game.batch.setProjectionMatrix(cam.combined); //keeps player sprite from doing weird out of sync movement
-
-		game.batch.begin(); //starts sprite spriteBatch
+		
+        mouse_position.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+        cam.unproject(mouse_position); //gets mouse coordinates within viewport
+        
+        game.batch.begin(); //starts sprite spriteBatch
 
         p1.draw(game.batch); //draws p1 sprite
+
         game.batch.end(); //starts sprite spriteBatch
 
 
         //mapRenderer.render(layerAfterBackground); //renders layer of Tiled that hides p1
-
-        
-		/*
-		//mouse x and y
-		int mX = Gdx.input.getX();
-		int mY = Gdx.graphics.getHeight() - Gdx.input.getY();
-		if (mY < 100 && mX < 100 && Gdx.input.isButtonPressed(Buttons.LEFT)) {
-			game.setScreen(new MainMenu(game));
-
-		}*/
 		
 	}
 
@@ -137,8 +125,8 @@ public class Level1 implements Screen{
 	@Override
 	public void resize(int width, int height) {
 		viewport.update(width, height, true); //updates the viewport camera
-		float pW = p1.getWidth() / cam.zoom; //Keeps p1 scaled
-		float pH = p1.getHeight() / cam.zoom; // ^
+		float pW = p1.getWidth(); //Keeps p1 scaled
+		float pH = p1.getHeight(); // ^
 		p1.setSize(pW, pH); // Keeps players size matched regardless of zoom
 	}
 	@Override
@@ -161,13 +149,15 @@ public class Level1 implements Screen{
 		map.dispose();
 		mapRenderer.dispose();
 		textureAtlas.dispose();
+		shapeRenderer.dispose();
 		
 
 	}
 
 	@Override
 	public void show() {
-		// TODO Auto-generated method stub
+
 		
 	}
+	
 }
--- libGDX Workspace/core/src/com/bpa/Player.java
+++ libGDX Workspace/core/src/com/bpa/Player.java
@@ -1,73 +1,60 @@
 package com.bpa;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 
 public class Player extends Sprite implements InputProcessor{
 
 	private Vector2 velocity = new Vector2();
-	private float speed = 150;
+	private float speed = 3;
 	private Circle boundingCircle;
+	private float angle;
 
-    public Player(Sprite sprite) {
+	
+	
+	
+	public Player(Sprite sprite) {
         
         //boundingCircle.set();
     	super(sprite);
-        boundingCircle = new Circle();
+    	
     }
 
 
     @Override
     public void draw(Batch batch) {
-    	update(Gdx.graphics.getDeltaTime());
+    	update(Gdx.graphics.getDeltaTime()); //allows movement of player
     	super.draw(batch);
     }
     
     public void update(float delta) {
 
-        // Set the circle's center to be (9, 6) with respect to the bird.
-        // Set the circle's radius to be 6.5f;
-        //boundingCircle.set(position.x + 9, position.y + 6, 6.5f);
-    	
-		if (Gdx.input.isKeyPressed(Input.Keys.W)) {
-			//p1.translateY(speed);
-			//cam.translate(0, .02f);
-			//p1.translate(0, speed); 
-			velocity.y = speed;
-		 	}
-        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
-        	//p1.translateX(speed);
-        	//p1.translate(speed, 0);
-        	velocity.x = speed;
-        } 
-        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-            //p1.translateX(-speed);
-        	//p1.translate(-speed, 0);
-        	velocity.x = -speed;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
-        	//p1.translate(0, -speed);
-        	//p1.translateY(-speed);
-        	velocity.y = -speed;
-        }
+
+        setX(getX() + velocity.x);
+        setY(getY() + velocity.y);
+
+
+
 
         
         
-        setX(getX() + velocity.x * delta);
-        setY(getY() + velocity.y * delta);
-
     }
     
     @Override
     public boolean keyDown(int keycode) {
+    	
     	switch(keycode) {
+    	
     	case Keys.W:
     		velocity.y = speed;
     		break;
@@ -137,8 +124,20 @@ public class Player extends Sprite implements InputProcessor{
 
 	@Override
 	public boolean mouseMoved(int screenX, int screenY) {
-		// TODO Auto-generated method stub
-		return false;
+
+		float mouseX = Level1.mouse_position.x; //grabs cam.unproject x vector value
+		float mouseY = Level1.mouse_position.y; //grabs cam.unproject y vector value
+		
+		angle = MathUtils.atan2(mouseY - getY(), mouseX - getX()) * MathUtils.radDeg;
+
+        angle = angle - 90;
+	    if (angle < 0) {
+	    	angle += 360 ;
+	    }
+       
+       setRotation(angle);
+		
+        return true;
 	}
 
 
@@ -148,7 +147,5 @@ public class Player extends Sprite implements InputProcessor{
 		return false;
 	}
 	
-    public Circle getBoundingCircle() {
-        return boundingCircle;
-    }
+	
 }
